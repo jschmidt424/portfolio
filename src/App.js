7ccae4c875d6741ef1c563d78ce17939e46ac6a6
spazdrum <spazdrum@gmail.com>
2021-09-07 12:36:35 -0400

    I'm bad at commits :)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import "tailwindcss/tailwind.css"
+
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <NavBar />
       <About />
       <Projects />
