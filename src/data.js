98fdc121e51d80966615f4a4c60d4985c888e151
spazdrum <spazdrum@gmail.com>
2023-09-23 16:35:33 -0400

    added deployment and github links

--- src/data.js
+++ src/data.js
@@ -1,19 +1,12 @@
 export const projects = [
   {
-    title: "Spotify Finder",
-    subtitle: "React.js",
-    description:
-      "This allows for a user to sign in with their, existing, spotify account and search for albums, songs and playlists.",
-    image: "./Spotify.png",
-    link: "https://spotify-song-search.netlify.app",
-  },
-  {
     title: "Simple Calculator",
     subtitle: "HTML, CSS, JavaScript",
     description:
       "A simple calculator that allows the user to perform basic math operations.",
     image: "./Calculator.png",
     link: "https://jschmidt424.github.io/calculator/",
+    github: "https://github.com/jschmidt424/calculator",
   },
   {
     title: "Rick and Morty Characters",
@@ -21,6 +14,7 @@ export const projects = [
     description: "This site pulls from the Rick and Morty APi to display the characters in the show and their information.",
     image: "./RickAndMorty.png",
     link: "https://randm-characters.netlify.app",
+    github: "https://github.com/jschmidt424/RickAndMorty",
   },
 ];
 
